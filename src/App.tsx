3d11d053fb9d502016861a4f25fe25bda9294250
charlespereira1 <charles.spereira-ext@viavarejo.com.br>
2021-03-01 13:32:06 -0300

    new structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Hello TikTok</Text>
+    </>
+  );
+};
+
+export default App;
