5cafc31f4c5e288f19ed45925f9a15041f68e463
charlespereira1 <charles.spereira-ext@viavarejo.com.br>
2021-03-01 14:20:32 -0300

    configure routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
 
 import Home from "../pages/Home";
 
 const Tabs = createBottomTabNavigator();
 
 const Routes: React.FC = () => (
-  <NavigationContainer>
-    <Tabs.Navigator>
-      <Tabs.Screen name="Home" component={Home} />
-    </Tabs.Navigator>
-  </NavigationContainer>
+  <Tabs.Navigator>
+    <Tabs.Screen name="Home" component={Home} />
+  </Tabs.Navigator>
 );
 
 export default Routes;
